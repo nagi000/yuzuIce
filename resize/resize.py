d654421a3fe09d39f86e37fae368235414f8d795
sironeko <sironeko1203@gmail.com>
2017-06-23 00:16:15 +0900

    first commit

---
+++ resize/resize.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+import sys
+import os
+
+#(dirpath,savepath)
+
+size = (250,250)
+dirpath = sys.argv[1]
+savepath = sys.argv[2]
+if dirpath[-1] != "/":
+	dirpath = dirpath + "/"
+if savepath[-1] != "/":
+	savepath = savepath + "/"
+
+if not os.path.exists(savepath):
+		os.makedirs(savepath)
+
+files = os.listdir(dirpath)
+
+i = 0
+for f in files:
+	filename = dirpath + f
+	img = cv2.imread(filename)
+	i += 1
+	resizeImg = cv2.resize(img,size)
+	cv2.imwrite(savepath + "train" + str(i) + ".jpg",resizeImg)
